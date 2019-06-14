3ba82e4753d8e139039333162800fd3fa261f91c
Julian Popescu <jpopesculian@gmail.com>
2019-06-14 14:43:20 +0200

    updated dotfiles

--- install/config.sh
+++ install/config.sh
@@ -6,13 +6,7 @@
 
 # before running ensure that there is a valid ssh key authorized for github
 
-cd $HOME
-
-sudo apt-add-repository -y ppa:martin-frost/thoughtbot-rcm
-sudo apt-add-repository -y ppa:tista/adapta
-sudo add-apt-repository -y ppa:snwh/pulp
-sudo add-apt-repository -y ppa:ricotz/docky
-sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+pushd $HOME
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
@@ -20,59 +14,58 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
-curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-add-repository -y ppa:martin-frost/thoughtbot-rcm
+sudo apt-add-repository -y ppa:tista/adapta
+sudo add-apt-repository -y ppa:snwh/pulp
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update
 
 sudo apt-get install -y \
-    rcm \
-    git \
-    nodejs \
-    build-essential \
-    neovim \
-    zsh \
-    tmux \
-    python-pip \
-    python3-pip \
-    unity-tweak-tool \
-    gnome-tweak-tool \
     adapta-gtk-theme \
-    paper-icon-theme \
-    plank \
-    redshift redshift-gtk \
     apt-transport-https \
+    build-essential \
     ca-certificates \
     curl \
-    software-properties-common \
     docker \
-    tmuxinator \
-    redis-server \
-    ruby \
-    golang-go \
+    gnome-tweak-tool \
     gnupg-agent \
-    pinentry-curses \
-    direnv \
+    google-chrome-stable \
     htop \
-    network-manager-openvpn-gnome \
+    neovim \
+    paper-icon-theme \
+    python-pip \
+    python3-pip \
+    rcm \
+    redis-server \
     silversearcher-ag \
-    google-chrome-stable
+    software-properties-common \
+    tmux \
+    tmuxinator \
+    unity-tweak-tool \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    zsh
 
-sudo npm install -g yarn
-sudo yarn global add hotel
-sudo yarn global add browser-sync
-sudo yarn global add prettier
-sudo yarn global add eslint
+sudo usermod -aG docker julian
+
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install node
+nvm use node
+npm install -g yarn
 
 # oh-my-zsh
 git clone git@github.com:robbyrussell/oh-my-zsh.git .oh-my-zsh
+mkdir -p ~/.oh-my-zsh/custom/themes/
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | sudo bash -s stable
-
-# powerline
-pip2 install --user git+git://github.com/powerline/powerline
-pip3 install --user git+git://github.com/powerline/powerline
 
 mkdir -p $HOME/.fonts
 mkdir -p $HOME/.config/fontconfig/conf.d
@@ -96,3 +89,5 @@ mkdir -p $HOME/Development
 
 # zsh shell
 chsh -s $(which zsh)
+
+popd
