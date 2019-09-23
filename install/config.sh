20cf1304a176c32d45fb95d2e1054d915456f1d2
Julian Popescu <jpopesculian@gmail.com>
2019-09-23 11:29:55 +0200

    udev helpers

--- install/config.sh
+++ install/config.sh
@@ -17,7 +17,7 @@ popd
 sudo apt-get update
 sudo apt-get install -y curl gnupg
 
-gpg --import ./install/pubkey.asc
+gpg --import ./install/assets/pubkey.asc
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
