eacc4f716d34fae277a63cc6bf542de55c979dcb
Julian Popescu <jpopesculian@gmail.com>
2019-09-23 13:07:26 +0200

    added docker-compose

--- install/config.sh
+++ install/config.sh
@@ -105,6 +105,9 @@ wget -O prettyping https://raw.githubusercontent.com/denilsonsa/prettyping/maste
 sudo chmod +x prettyping
 sudo mv prettyping /usr/bin
 
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 popd
 
 # kitty
